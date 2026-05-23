d889b139166251e92c2ce070d9da99f065d84c5a
dmice9 <daniil-kuznecov-06@mail.ru>
2026-05-23 19:14:08 +0300

    refactor(ml): удалить HTTP-эндпоинт и локальную инициализацию модели из app.py

--- ml/app.py
+++ ml/app.py
@@ -1,18 +1,9 @@
 import asyncio
 from fastapi import FastAPI
-from .model import load_model, predict
-from .schemas import PredictionRequest, PredictionResponse
+
 from .worker import run_consumer
 
 app = FastAPI(title="Autonotes ML Service")
-
-model = load_model()
-
-
-@app.post("/predict", response_model=PredictionResponse)
-def predict_text(request: PredictionRequest):
-    prediction = predict(model, request.text)
-    return PredictionResponse(prediction=prediction)
 
 
 @app.on_event("startup")
